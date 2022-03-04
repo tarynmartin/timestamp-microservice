4eea8a767b557949e0bd6d9b64c942dd9a3ff7fc
Taryn Martin <taryn@fairwinds.com>
2022-03-04 16:53:10 -0700

    Created microservice off of template

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // server.js
 // where your node app starts
 
+const dayjs = require('dayjs');
+var advancedFormat = require('dayjs/plugin/advancedFormat')
+dayjs.extend(advancedFormat)
+
 // init project
 var express = require('express');
 var app = express();
@@ -18,13 +22,26 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// adding ? to a parameter makes it optional
+app.get("/api/:date?", (req, res) => {
+  const newDate = new Date(req.params.date);
+  const parsed = dayjs(parseInt(req.params.date));
 
+  if (!req.params.date) {
+    res.json({ unix: dayjs().format('x'), utc: dayjs() });
+  } else if (!parsed.isValid()) {
+    res.json({ error: 'Invalid Date' });
+  } else if (req.params.date.includes('-')) {
+    res.json({ unix: dayjs(req.params.date).format('x'), utc: newDate });
+  } else {
+    res.json({ unix: req.params.date, utc: parsed })
+  }
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
